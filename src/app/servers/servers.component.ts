5450bff6da5a391d28ee946b1e4cbbebd74f4f45
mrunalpatil1542000 <mrunalpatil1542000@gmail.com>
2022-10-19 15:07:51 +0530

    Button Enabled on input field value & formatting.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,15 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = 'Testserver';
+  serverName = '';
+  //serverName = 'Testserver';
 
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
-  }
+  constructor() { }
+
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.allowNewServer = true;
+  //   }, 2000);
+  // }
 
   ngOnInit() {
   }
